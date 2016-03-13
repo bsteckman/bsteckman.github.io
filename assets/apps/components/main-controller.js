c60785f1ef2da393ccd31e09fa29a7daadb14cb3
Brett Steckman <eveningside@gmail.com>
2016-03-13 16:38:35 -0600

    attatches gohome function to resume page

---
+++ assets/apps/components/main-controller.js
@@ -0,0 +1,9 @@
+app.controller('MainController', function ($scope) {
+    $scope.goHome = function(){
+        window.location.href = "http://bsteckman.github.io";
+        window.location.reload();
+    }
+    
+        
+    
+});
