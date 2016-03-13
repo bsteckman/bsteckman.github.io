eb6b6f2116e86fb635fa537432f0b45569a1cb54
Brett Steckman <eveningside@gmail.com>
2016-03-13 16:54:05 -0600

    fixed issue with resume background.  maybe fixed loop for landing->resume->landing

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,7 +1,7 @@
 app.controller('MainController', function ($scope) {
     $scope.goHome = function(){
         window.location.href = "http://bsteckman.github.io/#";
-        window.location.reload();
+
     }
     
         
